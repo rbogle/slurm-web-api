249da0789cd0de590f0c0e2562a2c0677e2813a7
Rian Bogle <rbogle@usgs.gov>
2017-01-06 12:42:30 -0700

    split queue retrieval into factory and split data output into sep. panels

--- app/views/queue/queue.js
+++ app/views/queue/queue.js
@@ -2,22 +2,29 @@
 angular.module('app.queue', ['ngRoute']);
 angular.module('app.queue').config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/queue', {
-    templateUrl: 'views/queue/queue.html',
-    controller: 'QueueCtrl'
+    templateUrl: 'views/queue/queue.html'
   });
 }]);
 
-angular.module('app.queue').controller('QueueCtrl', function ($scope){
-  $scope.title = "Queue"
+angular.module('app.queue').factory('queue', function($http){
+    var url = "http://nebula.wr.usgs.gov/slurmapi/queue";
+    var queue ={};
+    queue.jobs = {};
+    queue.update = function(){
+      $http.get(url).then(function(response){
+          //console.log(response.status);
+          queue.jobs.running=response.data;
+      });
+      return queue.jobs;
+    };
+    return queue;
 });
 
-angular.module('app.queue').controller('ListCtrl', function ($http){
+angular.module('app.queue').controller('QueueCtrl', function (queue, $interval){
   var self = this;
-  // nebula allows CORS see enable-cors.org
-  var url = "http://nebula.wr.usgs.gov/slurmapi/queue";
-  $http.get(url).then(function(response){
-      console.log(response.status);
-      self.jobs = response.data;
-  });
+  self.jobs = queue.update();
+  $interval(function(){
+    self.jobs = queue.update();
+  }, 5000);
 
 });
