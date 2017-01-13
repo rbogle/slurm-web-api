72b1e196789a660e1d57693ba77db6e5aa4a8928
rian bogle <rbogle@usgs.gov>
2017-01-13 14:28:19 -0700

    filtering refactor

--- app/views/history/history.js
+++ app/views/history/history.js
@@ -23,56 +23,143 @@ angular.module('app.history').controller('HistoryCtrl', function ($scope, $http,
 
   var self = this;
   var url_base = "http://nebula.wr.usgs.gov/slurmapi/history";
-  self.list={};
-  self.filter_states=['JOB_PENDING','JOB_RUNNING','JOB_SUSPENDED','JOB_COMPLETE','JOB_CANCELLED','JOB_FAILED',
-  'JOB_TIMEOUT', 'JOB_NODE_FAIL', 'JOB_PREEMPTED', 'JOB_BOOT_FAIL', 'JOB_DEADLINE'];
-  self.filter_names=['limit','offset','user','job id','job name','state','start time','end time','submit time'];
-  self.filter_ops=['=','>','<','>=','<=', '!='];
-  self.filters_set={ limit: '25'};
-  self.filter_key=""
-  self.filter_val=""
+  self.data={};
+  self.data_labels=['Id', "Name", 'User', 'Account', 'Status', 'Partition', 'Nodes', 'CPUs', 'Memory', 'WallTime', 'Submit Time', 'Start Time', 'End Time'];
+
+  self.default_set=[
+    {'key': 'limit', 'val': '25', 'op': '='}
+  ];
+  self.filters_set = self.default_set.slice(0);
+  self.filter={
+    'key': '',
+    'val': '',
+    'op': '='
+  };
+  self.sortType='';
+  self.sortReverse=true;
+  self.filters={
+    'limit':{
+        ops: ['='],
+        valids: [],
+        type: 'int',
+        unique: true,
+        input: 'text'
+    },
+    'offset':{
+        ops: ['='],
+        valids: [],
+        type: 'int',
+        unique: true,
+        input: 'text'
+    },
+    'user':{
+        ops: ['=' ],
+        valids: [],
+        type: 'string',
+        unique: true,
+        input: 'text'
+    },
+    'job id':{
+        ops: ['=','>','<','>=','<=' ],
+        valids: [],
+        type: 'int',
+        unique: false,
+        input: 'text'
+    },
+    'job name':{
+        ops: ['='],
+        valids: [],
+        type: 'string',
+        unique: true,
+        input: 'text'
+    },
+    'state':{
+        ops: ['='],
+        valids: ['JOB_PENDING','JOB_RUNNING','JOB_SUSPENDED','JOB_COMPLETE','JOB_CANCELLED','JOB_FAILED',
+        'JOB_TIMEOUT', 'JOB_NODE_FAIL', 'JOB_PREEMPTED', 'JOB_BOOT_FAIL', 'JOB_DEADLINE'],
+        type: 'int',
+        unique: true,
+        input: 'select'
+    },
+    'submit time':{
+        ops: ['=', '>', '<', '>=', '<=' ],
+        valids: [],
+        unique: false,
+        type: 'datetime',
+        input: 'datetime'
+    },
+    'start time':{
+        ops: ['=', '>', '<', '>=', '<=' ],
+        valids: [],
+        unique: false,
+        type: 'datetime',
+        input: 'datetime'
+    },
+    'end time':{
+        ops: ['=', '>', '<', '>=', '<=' ],
+        valids: [],
+        unique: false,
+        type: 'datetime',
+        input: 'datetime'
+    }
+  };
 
   self.reset_filters = function(){
-    self.filters_set={ limit: '25'};
+    self.filters_set={};
+    self.filters_set=self.default_set.slice(0);
     self.update();
   };
 
-  self.add_filter= function (key,value){
-    if (self.filter_names.indexOf(key) !== -1){
-      console.log("adding "+key+" & "+value)
-      self.filters_set[key]=value;
-      self.update()
-    }
+  self.add_filter= function (key,val,op){
+      if (self.filters[key].unique){
+        for( i =0; i < self.filters_set.length; i++){
+            if(self.filters_set[i].key == key){
+              self.filters_set[i].op = op;
+              self.filters_set[i].val = val;
+              self.update();
+              return;
+            }
+        }
+      }
+      var filter= { 'key': key, 'val': val, 'op':op};
+      self.filters_set.push(filter);
+      self.update();
   };
 
   self.del_filter = function (key,value) {
-    //console.log("removing "+key+" & "+value)
-    if(key in self.filters_set){
-       delete self.filters_set[key];
-       self.update()
+
+     for (i=0; i< self.filters_set.length; i++){
+       var f = self.filters_set[i];
+       if(key == f.key){
+         self.filters_set.splice(i,1);
+         break;
+       }
     }
+    self.update()
   };
 
   self.update = function (){
     url = url_base;
-    if (Object.keys(self.filters_set).length >= 0){
+    if (self.filters_set.length>0){
       url+="?"
     }
-    for (key in self.filters_set) {
-      url += key+"="+self.filters_set[key]+"&";
+  //  console.log(self.filters_set)
+    for (filter of self.filters_set) {
+      //console.log(filter.key+filter.op+filter.val)
+      url += filter.key+filter.op+filter.val+"&";
     }
-    //console.log(url)
     $http.get(url).then(function(response){
         //console.log(response.status);
-        self.list = response.data
+        self.data = response.data
     });
   };
 
   self.show_input = function(type,filter){
+      //console.log('show_input', type, filter)
       var tree = {
         'text' : [ 'limit','offset','user','job id','job name'],
         'select' : ['state'],
-        'date' : ['start time','end time','submit time']
+        'datetime' : ['start time','end time','submit time']
       };
       if (tree[type].indexOf(filter) !== -1){
         return true;
